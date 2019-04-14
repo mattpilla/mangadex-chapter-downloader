82b2a4b194147faeac9f2a73d58305c6805b1cb6
mattpilla <kaztalek@gmail.com>
2019-04-14 18:48:09 -0400

    try to get chapter info on webpage

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 new Vue({
     el: '#app',
     data: {
-        url: ''
+        url: '',
+        error: ''
+    },
+    methods: {
+        getChapter() {
+            this.error = '';
+            if (!this.url) {
+                return;
+            }
+            let matches = /mangadex\.org\/chapter\/([^\/]+)/.exec(this.url);
+            if (!matches || matches.length < 2) {
+                return this.error = `Unable to find chapter ID from given URL. Enter the URL in a form like this: <b>https://mangadex.org/chapter/10122/1</b>`;
+            }
+            axios.get(`https://mangadex.org/api/chapter/${matches[1]}`).then(response => {
+                if (response.status === 200) {
+                    console.log(response.data);
+                    return response.data;
+                } else {
+                    console.log(response);
+                    return false;
+                }
+            }).catch(e => {
+                console.error(e);
+                return false;
+            });
+        }
     }
 });
