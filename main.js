4d2d5fab1b55324838378b9cd46f105522245b2f
mattpilla <kaztalek@gmail.com>
2019-04-14 21:36:54 -0400

    maybe cors solution

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ new Vue({
             if (!matches || matches.length < 2) {
                 return this.error = `Unable to find chapter ID from given URL. Enter the URL in a form like this: <b>https://mangadex.org/chapter/10122/1</b>`;
             }
-            axios.get(`https://mangadex.org/api/chapter/${matches[1]}`).then(response => {
+            axios.get(`https://cors.io/?https://mangadex.org/api/chapter/${matches[1]}`).then(response => {
                 if (response.status === 200) {
                     console.log(response.data);
                     return response.data;
